63313db46ace67a0cbf745487fa4fa9bf006ba0d
Reza <rezammdzd0@gmail.com>
2023-10-12 21:53:59 +0330

    axios and toast package added

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,88 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import CharacterList from "./components/CharacterList";
 import CharacterDetails from "./components/CharacterDetails";
-import { allCharacters } from "../data/data";
 import { useEffect, useState } from "react";
-
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
 function App() {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  //NOTE fetch
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("https://rickandmortyapi.com/api/character")
+  //     .then((res) => {
+  //       if (!res.ok) throw new Error("there was an error fetching!");
+
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       setCharacters(data.results.slice(0, 5));
+  //     })
+  //     .catch((err) => {
+  //       toast.error(err.message);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  //NOTE fetch
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       setIsLoading(true);
+  //       const res = await fetch("https://rickandmortyapi.com/api/character");
+  //       if (!res.ok) throw new Error("there was an error fetching!");
+  //       const data = await res.json();
+  //       setCharacters(data.results.slice(0, 5));
+  //     } catch (error) {
+  //       toast.error(error.message)
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
+  //NOTE Axios Async
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       setIsLoading(true);
+  //       const {data} = await axios.get("https://rickandmortyapi.com/api/character");
+
+  //       // console.log(res);
+  //       setCharacters(data.results.slice(0, 5));
+  //     } catch (error) {
+  //       toast.error(error.message)
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
+  //NOTE - Axios Promise
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://rickandmortyapi.com/api/character")
-      .then((res) => res.json())
-      .then((data) => setCharacters(data.results.slice(0, 5)));
-    setIsLoading(false);
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then(({ data }) => {
+        setCharacters(data.results.slice(0, 5));
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
-  // useEffect(()=>{
-
-  //   async function fetchData(){
-  //     setIsLoading(true)
-  //     const res = await fetch("https://rickandmortyapi.com/api/character")
-  //     const data = await res.json();
-  //     setCharacters(data.results.slice(0,5))
-  //     setIsLoading(false)
-  //   }
-  //   fetchData()
-  // } , [])
 
   return (
     <div className="app">
+      <Toaster position="top-right" reverseOrder={false} />
       <NavBar numOfResult={characters.length} />
       <Main>
         <CharacterList characters={characters} isLoading={isLoading} />
